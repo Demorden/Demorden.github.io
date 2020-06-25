5236286a355ceb085e864c40320a955fcc0f8303
Max Mordini <Demorden@users.noreply.github.com>
2020-06-25 19:11:46 +0200

    Add files via upload

---
+++ scripts/main.js
@@ -0,0 +1,41 @@
+
+$(function() {
+
+    // Populate using an object literal
+
+    // WTF.init({
+
+    //     heading: [
+    //         "It's a fucking",
+    //         "Check this shit out, a fucking"
+    //     ],
+    //     response: [
+    //         "Already fucking seen one",
+    //         "Give me a-fucking-nother one",
+    //         "Don't really give a fuck"
+    //     ],
+    //     template: [
+    //         "Big @color @animal",
+    //         "Silly @animal with @color fur"
+    //     ],
+    //     animal: [
+    //         "dog",
+    //         "cat",
+    //         "rabbit"
+    //     ],
+    //     color: [
+    //         "red",
+    //         "blue",
+    //         "green",
+    //         "yellow"
+    //     ]
+    // });
+
+    // Populate using a JSON file
+    // WTF.init( 'sample.json' );
+
+    // Populate using a Google spreadsheet ID (you must publish it first!)
+    // @see https://support.google.com/drive/answer/37579?hl=en
+    //WTF.init( '1d9PZDA416QuiAK2-gQeXMFJHRG2r1Qi0vT38IEWcXHY' );
+    WTF.init( '2PACX-1vR4IcznmWqW_O-9xdr7x6RwBh_-qkYnnvMqfsymtxcrZ0ONBtXJjFsr1-LWxlwy0XFWB8QRigMrMX2j' );
+});
